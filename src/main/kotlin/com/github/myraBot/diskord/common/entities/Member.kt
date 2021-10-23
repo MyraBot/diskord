18df8d2091b4cae83cb4bf06c85603c873d4dd28
Marian <toni_kukoc@gmx.de>
2021-10-23 17:16:07 +0200

    :bug: Fixed timestamp types

--- src/main/kotlin/com/github/myraBot/diskord/common/entities/Member.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/entities/Member.kt
@@ -4,14 +4,15 @@ import com.github.myraBot.diskord.utilities.InstantSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import java.sql.Timestamp
+import java.time.Instant
 
 @Serializable
 data class Member(
         val nick: String? = null,
         val avatar: String? = null,
         val roles: Array<String>,
-        @SerialName("joined_at") @Serializable(with = InstantSerializer::class) val joinedAt: Timestamp,
-        @SerialName("premium_since") @Serializable(with = InstantSerializer::class) val premiumSince: Timestamp? = null,
+        @SerialName("joined_at") @Serializable(with = InstantSerializer::class) val joinedAt: Instant,
+        @SerialName("premium_since") @Serializable(with = InstantSerializer::class) val premiumSince: Instant? = null,
         val deaf: Boolean,
         val mute: Boolean,
         val pending: Boolean = false,
