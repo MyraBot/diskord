b2dab0631780c8514ab257a913e0e9ff70e169a9
Marian <toni_kukoc@gmx.de>
2021-10-23 11:04:55 +0200

    :lock: Made constructor parameters private

--- src/main/kotlin/com/github/myraBot/diskord/rest/Route.kt
+++ src/main/kotlin/com/github/myraBot/diskord/rest/Route.kt
@@ -1,16 +1,14 @@
-package com.github.myraBot.diskord.endpoints
+package com.github.myraBot.diskord.rest
 
 import com.github.m5rian.discord.*
-import com.github.myraBot.diskord.rest.Endpoints
-import com.github.myraBot.diskord.rest.RouteArguments
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import kotlinx.serialization.KSerializer
 
-class Route<R>(val httpMethod: HttpMethod, val path: String, val serializer: KSerializer<R>) {
+class Route<R>(private val httpMethod: HttpMethod, private val path: String, private val serializer: KSerializer<R>) {
 
-    suspend fun execute(json: String, argBuilder: RouteArguments.() -> Unit = {}): R {
+    suspend fun execute (json: String, argBuilder: RouteArguments.() -> Unit = {}): R {
         val args = RouteArguments().apply(argBuilder).entries
         var route = Endpoints.baseUrl + path
         args.forEach { route = route.replace("{${it.first}}", it.second.toString()) }
