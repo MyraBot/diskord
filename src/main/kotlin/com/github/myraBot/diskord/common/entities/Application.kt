6ed6a280a3696c7f7bcd6054516104686942ff5b
Marian <toni_kukoc@gmx.de>
2021-10-23 17:33:56 +0200

    :bust_in_silhouette: Added getUser endpoint

--- src/main/kotlin/com/github/myraBot/diskord/common/entities/Application.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/entities/Application.kt
@@ -1,8 +1,11 @@
 package com.github.myraBot.diskord.common.entities
 
+import com.github.myraBot.diskord.rest.Endpoints
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Application(
         val id: String
-)
+) {
+    suspend fun getUser(id: String) = Endpoints.getUser.execute { arg("user.id", id) }
+}
--- src/main/kotlin/com/github/myraBot/diskord/rest/Endpoints.kt
+++ src/main/kotlin/com/github/myraBot/diskord/rest/Endpoints.kt
@@ -1,5 +1,6 @@
 package com.github.myraBot.diskord.rest
 
+import com.github.m5rian.discord.objects.entities.User
 import com.github.myraBot.diskord.common.entities.Application
 import com.github.myraBot.diskord.common.entities.Member
 import com.github.myraBot.diskord.common.entities.Message
@@ -12,6 +13,7 @@ object Endpoints {
 
     val createMessage = Route<Message>(HttpMethod.Post, "/channels/{channel.id}/messages", Message.serializer())
     val getChannel = Route<Channel>(HttpMethod.Get, "/channels/{channel.id}")
+    val getUser = Route<User>(HttpMethod.Get, "/users/{user.id}", User.serializer())
     val getGuildMember = Route<Member>(HttpMethod.Get, "/guilds/{guild.id}/members/{user.id}", Member.serializer())
     val getBotApplication = Route<Application>(HttpMethod.Get, "/oauth2/applications/@me", Application.serializer())
 }
