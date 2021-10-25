dee5506bce821474cabed44152b90a8a64814073
Marian <toni_kukoc@gmx.de>
2021-10-25 13:32:24 +0200

    :rotating_light: Fixed imports

--- src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/ApplicationBehavior.kt
+++ src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/ApplicationBehavior.kt
@@ -1,9 +1,10 @@
 package com.github.myraBot.diskord.rest.behaviors
 
+import com.github.myraBot.diskord.common.entities.User
 import com.github.myraBot.diskord.rest.Endpoints
 
 interface ApplicationBehavior {
 
-    suspend fun getUser(id: String) = Endpoints.getUser.execute { arg("user.id", id) }
+    suspend fun getUser(id: String): User = User(Endpoints.getUser.execute { arg("user.id", id) })
 
 }
