fbb2a3daaf750b5865c163f180497f820518a725
Marian <toni_kukoc@gmx.de>
2021-10-25 13:28:19 +0200

    :credit_card: Added guild and id reference to Member

--- src/main/kotlin/com/github/myraBot/diskord/common/entities/Member.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/entities/Member.kt
@@ -1,11 +1,14 @@
 package com.github.myraBot.diskord.common.entities
 
+import com.github.m5rian.discord.objects.entities.UserData
 import com.github.myraBot.diskord.common.entityData.GuildData
 import com.github.myraBot.diskord.common.entityData.MemberData
 
 class Member(
-        val guildData: GuildData,
-        val memberData: MemberData
+        private val guildData: GuildData,
+        private val memberData: MemberData,
+        private val userData: UserData,
 ) {
-
+    val id: String = userData.id
+    val guild: Guild = Guild(guildData)
 }
