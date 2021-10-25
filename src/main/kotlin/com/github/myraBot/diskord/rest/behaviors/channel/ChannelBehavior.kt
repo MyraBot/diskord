5ee793eb1bc9c2481001d47c6c996a93dbd56ebf
Marian <toni_kukoc@gmx.de>
2021-10-25 13:31:49 +0200

    :speech_balloon: Added ChannelBehavior#send

--- src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/channel/ChannelBehavior.kt
+++ src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/channel/ChannelBehavior.kt
@@ -1,6 +1,18 @@
 package com.github.myraBot.diskord.rest.behaviors.channel
 
+import com.github.m5rian.discord.JSON
+import com.github.myraBot.diskord.common.entities.Message
+import com.github.myraBot.diskord.rest.Endpoints
 import com.github.myraBot.diskord.rest.behaviors.Entity
+import com.github.myraBot.diskord.rest.builders.MessageBuilder
+import kotlinx.serialization.encodeToString
 
 interface ChannelBehavior : Entity {
+
+    suspend fun send(message: MessageBuilder.() -> Unit): Message {
+        val json = JSON.encodeToString(MessageBuilder().apply(message))
+        val messageData = Endpoints.createMessage.execute(json) { arg("channel.id", id) }
+        return Message(messageData)
+    }
+
 }
