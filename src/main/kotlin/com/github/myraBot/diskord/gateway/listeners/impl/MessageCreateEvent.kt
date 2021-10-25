303a86213ee83108e52335f61e0398bc91da7341
Marian <toni_kukoc@gmx.de>
2021-10-25 13:26:23 +0200

    :bento: Added more data to MessageCreateEvent

--- src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/MessageCreateEvent.kt
+++ src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/MessageCreateEvent.kt
@@ -4,5 +4,9 @@ import com.github.myraBot.diskord.common.entities.Message
 import com.github.myraBot.diskord.gateway.listeners.Event
 
 data class MessageCreateEvent(
-        val message: Message
-) : Event()
+        val message: Message,
+) : Event() {
+    val guild = message.guild
+    val member = message.member
+    val channel = message.channel
+}
