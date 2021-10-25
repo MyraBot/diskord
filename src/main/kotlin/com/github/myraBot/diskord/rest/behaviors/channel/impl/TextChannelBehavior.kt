da074c604a8e9f71d83336e8a3aa3ce2e15540f6
Marian <toni_kukoc@gmx.de>
2021-10-25 13:34:13 +0200

    :fire: Removed TextChannelBehavior#send

--- src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/channel/impl/TextChannelBehavior.kt
+++ src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/channel/impl/TextChannelBehavior.kt
@@ -1,18 +1,7 @@
 package com.github.myraBot.diskord.rest.behaviors.channel.impl
 
-import com.github.m5rian.discord.JSON
-import com.github.myraBot.diskord.common.entityData.message.MessageData
-import com.github.myraBot.diskord.rest.Endpoints
 import com.github.myraBot.diskord.rest.behaviors.channel.ChannelBehavior
-import com.github.myraBot.diskord.rest.builders.MessageBuilder
-import kotlinx.serialization.encodeToString
 
 interface TextChannelBehavior : ChannelBehavior {
-
-    suspend fun send(builder: MessageBuilder.() -> Unit): MessageData {
-        val message = MessageBuilder().apply(builder)
-        val json = JSON.encodeToString(message)
-        return Endpoints.createMessage.execute(json) { arg("channel.id", id) }
-    }
 
 }
