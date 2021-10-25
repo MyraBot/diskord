86dce863d81eb7349d70d8816231409b035b52f7
Marian <toni_kukoc@gmx.de>
2021-10-25 13:38:16 +0200

    :sparkles: Added user and content values

--- src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/MessageCreateEvent.kt
+++ src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/MessageCreateEvent.kt
@@ -6,7 +6,9 @@ import com.github.myraBot.diskord.gateway.listeners.Event
 data class MessageCreateEvent(
         val message: Message,
 ) : Event() {
+    val content = message.content
+    val user = message.user
+    val channel = message.channel
     val guild = message.guild
     val member = message.member
-    val channel = message.channel
 }
