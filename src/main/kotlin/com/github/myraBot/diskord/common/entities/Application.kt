1988abeaa7d21e31c984a391e73462a1a285026d
Marian <toni_kukoc@gmx.de>
2021-10-26 19:57:52 +0200

    :coffin: Removed ApplicationBehavior

--- src/main/kotlin/com/github/myraBot/diskord/common/entities/Application.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/entities/Application.kt
@@ -1,8 +1,7 @@
 package com.github.myraBot.diskord.common.entities
 
 import com.github.myraBot.diskord.common.entityData.ApplicationData
-import com.github.myraBot.diskord.rest.behaviors.ApplicationBehavior
 
 class Application(
         val data: ApplicationData
-)  : ApplicationBehavior
+)
