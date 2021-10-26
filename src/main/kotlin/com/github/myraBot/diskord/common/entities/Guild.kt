1ae73f4553ca714eeb19fbaaa9391ad1e7f68ec0
Marian <toni_kukoc@gmx.de>
2021-10-26 19:20:49 +0200

    :bug: Made the Guild implement Entity & Fixed Guild#getMember

--- src/main/kotlin/com/github/myraBot/diskord/common/entities/Guild.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/entities/Guild.kt
@@ -1,9 +1,10 @@
 package com.github.myraBot.diskord.common.entities
 
 import com.github.myraBot.diskord.common.entityData.GuildData
+import com.github.myraBot.diskord.rest.behaviors.GuildBehavior
 
 class Guild(
         val data: GuildData
-) {
-    val id: String = data.id
+) : GuildBehavior {
+    override val id: String = data.id
 }
--- src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/GuildBehavior.kt
+++ src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/GuildBehavior.kt
@@ -3,7 +3,7 @@ package com.github.myraBot.diskord.rest.behaviors
 import com.github.myraBot.diskord.common.entityData.MemberData
 import com.github.myraBot.diskord.rest.Endpoints
 
-interface GuildBehavior : GetTextChannelBehavior {
-    suspend fun getMember(id: String) = Endpoints.getGuildMember.execute { arg("user.id", id) }
+interface GuildBehavior : Entity, GetTextChannelBehavior {
+    suspend fun getMember(id: String) = Endpoints.getGuildMember.execute {arg("guild.id", this@GuildBehavior.id); arg("user.id", id) }
     suspend fun botMember(): MemberData = getMember(Endpoints.getBotApplication.execute().id)
 }
