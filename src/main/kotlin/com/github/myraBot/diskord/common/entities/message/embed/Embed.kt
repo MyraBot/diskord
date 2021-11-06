df4a6bf0a7f823224f8d08c80d312c1349441892
Marian <toni_kukoc@gmx.de>
2021-11-06 14:03:41 +0100

    :page_facing_up: Added support for embed fields

--- src/main/kotlin/com/github/myraBot/diskord/common/entities/message/embed/Embed.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/entities/message/embed/Embed.kt
@@ -12,15 +12,19 @@ data class Embed(
         var title: String? = null,
         var type: String? = null,
         var description: String? = null,
-
         var url: String? = null,
         @Serializable(with = InstantSerializer::class) val timestamp: Instant? = null,
         @SerialName("colour") @Serializable(with = ColourSerializer::class) var colour: Color? = null,
-
         var footer: Footer? = null,
         var image: String? = null,
         var thumbnail: Thumbnail? = null,
         var author: Author? = null,
-)
+        val fields: MutableList<Field> = mutableListOf()
+) {
+    fun addField(data: Field.() -> Unit) {
+        val field = Field("", "").also { data.invoke(it) }
+        fields.add(field)
+    }
+}
 
 suspend fun embed(builder: suspend Embed.() -> Unit): Embed = Embed().apply { builder.invoke(this) }
