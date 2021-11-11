4a757156f61297b8b9ab7efd62a39015ac4934b3
Marian <toni_kukoc@gmx.de>
2021-11-11 20:39:17 +0100

    :sparkles: Added ListSerializer

--- src/main/kotlin/com/github/myraBot/diskord/utilities/Serializers.kt
+++ src/main/kotlin/com/github/myraBot/diskord/utilities/Serializers.kt
@@ -6,6 +6,8 @@ import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.jsonObject
 import java.awt.Color
 import java.text.SimpleDateFormat
 import java.time.Instant
@@ -23,11 +25,24 @@ object InstantSerializer : KSerializer<Instant> {
     override fun deserialize(decoder: Decoder): Instant {
         return Instant.parse(decoder.decodeString())
     }
-
 }
 
 object ColourSerializer : KSerializer<Color> {
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Color", PrimitiveKind.INT)
     override fun serialize(encoder: Encoder, value: Color) = encoder.encodeInt(value.rgb)
     override fun deserialize(decoder: Decoder): Color = Color.decode(decoder.decodeInt().toString())
+}
+
+class ListSerializer<T>(val serializer: KSerializer<T>) : KSerializer<List<T>> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(serializer.descriptor.serialName, PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): List<T> {
+        val roles = decoder.decodeSerializableValue(JsonArray.serializer())
+        return roles.map { JSON.decodeFromJsonElement(serializer, it.jsonObject) }
+    }
+
+    override fun serialize(encoder: Encoder, value: List<T>) {
+        val jsonArray = JsonArray(value.map { JSON.encodeToJsonElement(serializer, it) })
+        encoder.encodeString(jsonArray.toString())
+    }
 }
