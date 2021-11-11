9bb3423ed43ffe006fd3c8e983b038de8e2a8637
Marian <toni_kukoc@gmx.de>
2021-11-11 18:10:15 +0100

    :hammer: Added missing values to Role

--- src/main/kotlin/com/github/myraBot/diskord/common/entities/Role.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/entities/Role.kt
@@ -1,8 +1,20 @@
 package com.github.myraBot.diskord.common.entities
 
+import com.github.myraBot.diskord.utilities.ColourSerializer
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import java.awt.Color
 
 @Serializable
 data class Role(
-        val id: String
+        val id: String,
+        val name: String,
+        @Serializable(with = ColourSerializer::class) val color: Color,
+        @SerialName("hoist") val isShownSeparate: Boolean,
+        val icon: String? = null,
+        @SerialName("unicode_emoji") val unicodeEmoji: String? = null,
+        val position: Int,
+        val permissions: List<Permission>,
+        @SerialName("managed") val fromAnIntegration: Boolean,
+        val mentionable: Boolean
 )
