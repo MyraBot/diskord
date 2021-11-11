44b47f3ad5415a262c37704244946019c4717596
Marian <toni_kukoc@gmx.de>
2021-11-11 20:45:56 +0100

    :sparkles: Implemented Entity in Member & Added Member#colour & Added Member#getRoles

--- src/main/kotlin/com/github/myraBot/diskord/common/entities/guild/Member.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/entities/guild/Member.kt
@@ -1,14 +1,18 @@
 package com.github.myraBot.diskord.common.entities.guild
 
+import com.github.myraBot.diskord.common.entities.Role
 import com.github.myraBot.diskord.common.entities.User
+import com.github.myraBot.diskord.rest.behaviors.Entity
 import com.github.myraBot.diskord.utilities.InstantSerializer
 import com.github.myraBot.diskord.utilities.JSON
 import com.github.myraBot.diskord.utilities.Mention
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.*
+import java.awt.Color
 import java.time.Instant
 
+@Suppress("ArrayInDataClass")
 @Serializable
 data class MemberData(
         val user: User? = null,
@@ -23,20 +27,28 @@ data class MemberData(
         val permissions: String? = null
 )
 
+@Suppress("ArrayInDataClass")
 @Serializable
 data class Member(
         val guildId: String,
         val user: User,
         val nick: String? = null,
         val avatar: String? = null,
-        val roles: Array<String>,
+        @SerialName("roles") val roleIds: Array<String>,
         @SerialName("joined_at") @Serializable(with = InstantSerializer::class) val joinedAt: Instant,
         @SerialName("premium_since") @Serializable(with = InstantSerializer::class) val premiumSince: Instant? = null,
         val deaf: Boolean,
         val mute: Boolean,
         val pending: Boolean = false,
         val permissions: String? = null
-) {
+) : Entity {
+    override val id: String = user.id
+    val name: String get() = nick ?: user.username
+    val asMention: String = Mention.user(id)
+    val guild: SimpleGuild = SimpleGuild(this.guildId)
+    suspend fun getRoles(): List<Role> = guild.getRoles().filter { roleIds.contains(it.id) }
+    suspend fun getColour(): Color = getRoles().first().color
+
     companion object {
         fun withUser(member: MemberData, guild: SimpleGuild, user: User): Member {
             val jsonMember = JSON.encodeToJsonElement(member).jsonObject
@@ -58,9 +70,4 @@ data class Member(
                 .let { JSON.decodeFromJsonElement(it) }
         }
     }
-
-    val id: String = user.id
-    val name: String get() = nick ?: user.username
-    val asMention: String = Mention.user(id)
-    val guild: SimpleGuild = SimpleGuild(this.guildId)
 }
