7ae5ee21da1599bccd3e996bf5843a311c6e0a11
Marian <toni_kukoc@gmx.de>
2021-11-14 14:47:45 +0100

    :wheelchair: Made GetTextChannelBehavior#getChannel using caching & return normal Channels too

--- src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/GetTextChannelBehavior.kt
+++ src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/GetTextChannelBehavior.kt
@@ -1,16 +1,18 @@
 package com.github.myraBot.diskord.rest.behaviors
 
+import com.github.myraBot.diskord.common.caching.ChannelCache
+import com.github.myraBot.diskord.common.entities.Channel
 import com.github.myraBot.diskord.common.entities.channel.TextChannel
-import com.github.myraBot.diskord.rest.Endpoints
 
 interface GetTextChannelBehavior {
 
 }
 
-suspend inline fun <reified T> GetTextChannelBehavior.getChannel(id: String): T? {
-    val data = Endpoints.getChannel.execute { arg("channel.id", id) } ?: return null
+inline fun <reified T> GetTextChannelBehavior.getChannel(id: String): T? {
+    val channel = ChannelCache[id] ?: return null
     return when (T::class) {
-        TextChannel::class -> TextChannel(data)
+        Channel::class -> channel
+        TextChannel::class -> TextChannel(channel)
         else -> throw IllegalStateException()
     } as T
 }
