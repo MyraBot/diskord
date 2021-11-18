5b6c4ee2b3808575d6211cc2f4d7c620fe8b6af1
Marian <toni_kukoc@gmx.de>
2021-11-18 19:20:27 +0100

    :page_facing_up: Added guild and channel value to SlashCommandEvent

--- src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/interactions/SlashCommandEvent.kt
+++ src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/interactions/SlashCommandEvent.kt
@@ -1,7 +1,11 @@
 package com.github.myraBot.diskord.gateway.listeners.impl.interactions
 
+import com.github.myraBot.diskord.common.caching.ChannelCache
+import com.github.myraBot.diskord.common.caching.GuildCache
 import com.github.myraBot.diskord.common.entities.applicationCommands.Interaction
 import com.github.myraBot.diskord.common.entities.applicationCommands.slashCommands.SlashCommand
+import com.github.myraBot.diskord.common.entities.channel.TextChannel
+import com.github.myraBot.diskord.common.entities.guild.Guild
 import com.github.myraBot.diskord.common.entities.guild.Member
 import com.github.myraBot.diskord.gateway.listeners.Event
 import com.github.myraBot.diskord.rest.behaviors.InteractionCreateBehavior
@@ -15,4 +19,6 @@ data class SlashCommandEvent(
 ) : Event(), InteractionCreateBehavior {
     val command: SlashCommand get() = JSON.decodeFromJsonElement(interaction.interactionDataJson!!)
     val member: Member get() = Member.withUserInMember(interaction.member!!, interaction.guildId!!)
+    val guild: Guild get() = GuildCache[interaction.guildId!!]!!
+    val channel: TextChannel get() = ChannelCache.getAs<TextChannel>(interaction.channelId!!)!!
 }
