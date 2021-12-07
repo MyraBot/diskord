be72f3df9254558dc34ae935a65db4a3459cd105
Marian <toni_kukoc@gmx.de>
2021-12-07 18:03:01 +0100

    :bug: Fixed role mention

--- src/main/kotlin/com/github/myraBot/diskord/utilities/Mention.kt
+++ src/main/kotlin/com/github/myraBot/diskord/utilities/Mention.kt
@@ -3,6 +3,6 @@ package com.github.myraBot.diskord.utilities
 object Mention {
     fun user(id: String): String = "<@$id>"
     fun channel(id: String): String = "<#$id>"
-    fun role(id: String): String = "<&@$id>"
+    fun role(id: String): String = "<@&$id>"
     fun emoji(name: String, id: String, animated: Boolean): String = if (animated) "<a:$name:$id>" else "<:$name:$id>"
 }
