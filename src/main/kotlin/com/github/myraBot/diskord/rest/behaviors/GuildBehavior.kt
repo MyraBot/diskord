1906780973e6a57b27bc915f45108764bc16731d
Marian <elias-marian@web.de>
2021-12-17 14:08:35 +0100

    :zap: Guild#getMember uses the cache now

--- src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/GuildBehavior.kt
+++ src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/GuildBehavior.kt
@@ -1,5 +1,6 @@
 package com.github.myraBot.diskord.rest.behaviors
 
+import com.github.myraBot.diskord.common.caching.MemberCache
 import com.github.myraBot.diskord.common.caching.RoleCache
 import com.github.myraBot.diskord.common.entities.Channel
 import com.github.myraBot.diskord.common.entities.Role
@@ -8,14 +9,7 @@ import com.github.myraBot.diskord.rest.Endpoints
 
 interface GuildBehavior : Entity, GetTextChannelBehavior {
 
-    suspend fun getMember(id: String): Member? {
-        val memberData = Endpoints.getGuildMember.execute {
-            arg("guild.id", this@GuildBehavior.id)
-            arg("user.id", id)
-        }
-        return memberData?.let { Member.withUserInMember(it, this.id) }
-    }
-
+    suspend fun getMember(id: String): Member? = MemberCache[this.id, id]
     suspend fun getBotMember(): Member = getMember(Endpoints.getBotApplication.executeNonNull().id)!!
     suspend fun getRoles(): List<Role> = Endpoints.getRoles.executeNonNull { arg("guild.id", this@GuildBehavior.id) }
     suspend fun getRole(id: String): Role? = RoleCache[this.id, id]
