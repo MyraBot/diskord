551a6b6b9bd7bc2a86806be32b864502a2ed6849
Marian <elias-marian@web.de>
2022-01-02 21:28:36 +0100

    :lock: Added not nullable channels

--- src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/guild/voice/VoiceJoinEvent.kt
+++ src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/guild/voice/VoiceJoinEvent.kt
@@ -1,6 +1,9 @@
 package com.github.myraBot.diskord.gateway.listeners.impl.guild.voice
 
+import com.github.myraBot.diskord.common.entities.channel.VoiceChannel
 import com.github.myraBot.diskord.common.entities.guild.voice.VoiceState
 import com.github.myraBot.diskord.gateway.listeners.Event
 
-data class VoiceJoinEvent(val voiceState: VoiceState) : Event()
+data class VoiceJoinEvent(val voiceState: VoiceState) : Event() {
+    val channel: VoiceChannel get() = voiceState.channel!!
+}
--- src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/guild/voice/VoiceLeaveEvent.kt
+++ src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/guild/voice/VoiceLeaveEvent.kt
@@ -1,8 +1,11 @@
 package com.github.myraBot.diskord.gateway.listeners.impl.guild.voice
 
+import com.github.myraBot.diskord.common.entities.channel.VoiceChannel
 import com.github.myraBot.diskord.common.entities.guild.voice.VoiceState
 import com.github.myraBot.diskord.gateway.listeners.Event
 
 data class VoiceLeaveEvent(
         val voiceState: VoiceState,
-) : Event()
+) : Event() {
+    val channel: VoiceChannel get() = voiceState.channel!!
+}
