6d8ed252aff1769b5eee9e9e4e262a71d5dcb15c
Marian <elias-marian@web.de>
2022-01-04 17:08:23 +0100

    :art: Moved VoiceChannel#members out of the constructor

--- src/main/kotlin/com/github/myraBot/diskord/common/entities/channel/VoiceChannel.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/entities/channel/VoiceChannel.kt
@@ -1,15 +1,16 @@
 package com.github.myraBot.diskord.common.entities.channel
 
 import com.github.myraBot.diskord.common.caching.GuildCache
+import com.github.myraBot.diskord.common.caching.VoiceStateCache
 import com.github.myraBot.diskord.common.entities.Channel
 import com.github.myraBot.diskord.common.entities.guild.Guild
-import com.github.myraBot.diskord.common.entities.guild.voice.VoiceState
+import com.github.myraBot.diskord.common.entities.guild.Member
 
 data class VoiceChannel(
-        private val data: Channel,
-        val members: List<VoiceState>
+        private val data: Channel
 ) {
     val id: String = data.id
     val name: String = data.name
     val guild: Guild? get() = data.guildId?.let { GuildCache[it] }
+    val members: List<Member> get() = VoiceStateCache[id]!!.map { it.member!! }
 }
