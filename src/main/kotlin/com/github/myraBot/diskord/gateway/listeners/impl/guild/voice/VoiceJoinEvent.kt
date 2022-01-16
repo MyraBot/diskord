fb361b4e3a238978cb05bbebd161d448a9a71bf7
Marian <elias-marian@web.de>
2022-01-16 16:35:22 +0100

    :zap: VoiceJoinEvent#channel is now an executed Promise

--- src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/guild/voice/VoiceJoinEvent.kt
+++ src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/guild/voice/VoiceJoinEvent.kt
@@ -3,8 +3,8 @@ package com.github.myraBot.diskord.gateway.listeners.impl.guild.voice
 import com.github.myraBot.diskord.common.entities.channel.VoiceChannel
 import com.github.myraBot.diskord.common.entities.guild.voice.VoiceState
 import com.github.myraBot.diskord.gateway.listeners.Event
-import com.github.myraBot.diskord.rest.request.Promise
+import kotlinx.coroutines.runBlocking
 
 data class VoiceJoinEvent(val voiceState: VoiceState) : Event() {
-    val channel: Promise<VoiceChannel> get() = voiceState.channel
+    val channel: VoiceChannel get() = runBlocking { voiceState.getChannel().awaitNonNull() }
 }
