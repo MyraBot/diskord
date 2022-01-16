39a2982453845d518b1879e23dd6a4245cd62353
Marian <elias-marian@web.de>
2022-01-16 17:21:30 +0100

    :label: Added VoiceChannel#members field

--- src/main/kotlin/com/github/myraBot/diskord/common/entities/channel/VoiceChannel.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/entities/channel/VoiceChannel.kt
@@ -3,15 +3,22 @@ package com.github.myraBot.diskord.common.entities.channel
 import com.github.myraBot.diskord.common.entities.guild.Member
 import com.github.myraBot.diskord.common.voiceCache
 import com.github.myraBot.diskord.rest.request.Promise
+import kotlinx.coroutines.runBlocking
 
 data class VoiceChannel(
         override val data: ChannelData,
-) : Channel {
+) : GuildChannel {
 
-    suspend fun getMembers(): Promise<List<Member>> {
+    fun getMembers(): Promise<List<Member>> {
         return voiceCache[data.id].map { states ->
-            states?.map { it.member.awaitNonNull() } ?: emptyList()
+            states?.map {
+                println("retrieving member")
+                println("member = ${it.userId}")
+                it.member.awaitNonNull()
+            } ?: emptyList<Member>().also { println("returning empty list") }
         }
     }
+
+    val members: List<Member> get() = runBlocking { getMembers().awaitNonNull() }
 
 }
