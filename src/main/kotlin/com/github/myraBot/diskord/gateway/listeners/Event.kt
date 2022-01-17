89607d1b9b2f9c666b0a3d1043536276a06d2080
Marian <elias-marian@web.de>
2022-01-17 21:25:34 +0100

    :zap: Made events run in separate coroutines

--- src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/Event.kt
+++ src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/Event.kt
@@ -2,9 +2,15 @@ package com.github.myraBot.diskord.gateway.listeners
 
 import com.github.myraBot.diskord.common.Diskord
 import com.github.myraBot.diskord.rest.behaviors.DefaultBehavior
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.launch
+import java.util.concurrent.ForkJoinPool
 import kotlin.reflect.full.callSuspend
 import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.valueParameters
+
+private val scope: CoroutineScope = CoroutineScope(ForkJoinPool().asCoroutineDispatcher())
 
 abstract class Event : DefaultBehavior {
 
@@ -18,7 +24,9 @@ abstract class Event : DefaultBehavior {
     private suspend fun runFunctions(listener: EventListener) = listener.functions
         .filter { it.findAnnotation<ListenTo>()?.event == this::class }
         .forEach {
-            if (it.valueParameters.isEmpty()) it.callSuspend(listener)
-            else it.callSuspend(listener, this)
+            scope.launch {
+                if (it.valueParameters.isEmpty()) it.callSuspend(listener)
+                else it.callSuspend(listener, this@Event)
+            }
         }
 }
