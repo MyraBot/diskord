9f17e30efc407b0c7999c849f4bc423e3f5dea29
Marian <elias-marian@web.de>
2022-01-18 17:23:23 +0100

    :sparkles: Added Interaction#memberf

--- src/main/kotlin/com/github/myraBot/diskord/common/entities/applicationCommands/Interaction.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/entities/applicationCommands/Interaction.kt
@@ -4,6 +4,7 @@ import com.github.myraBot.diskord.common.JSON
 import com.github.myraBot.diskord.common.Optional
 import com.github.myraBot.diskord.common.entities.Locale
 import com.github.myraBot.diskord.common.entities.User
+import com.github.myraBot.diskord.common.entities.guild.Member
 import com.github.myraBot.diskord.common.entities.guild.MemberData
 import com.github.myraBot.diskord.common.entities.message.Message
 import kotlinx.serialization.SerialName
@@ -22,7 +23,7 @@ data class Interaction(
         @SerialName("data") val interactionDataJson: Optional<JsonObject> = Optional.Missing(),
         @SerialName("guild_id") val guildId: Optional<String> = Optional.Missing(),
         @SerialName("channel_id") val channelId: Optional<String> = Optional.Missing(),
-        val member: Optional<MemberData> = Optional.Missing(),
+        @SerialName("member") val memberData: Optional<MemberData> = Optional.Missing(),
         val user: Optional<User> = Optional.Missing(),
         val token: String,
         val version: Int,
@@ -31,4 +32,5 @@ data class Interaction(
         @SerialName("guild_locale") val guildLocale: Optional<Locale> = Optional.Missing(),
 ) {
     val interactionComponentData: InteractionComponentData? get() = interactionDataJson.value?.let { JSON.decodeFromJsonElement(it) }
+    val member: Member? get() = memberData.value?.let { Member.withUserInMember(it, guildId.forceValue) }
 }
