f5166fe8543472535f144aa6df4c456c942da61a
Marian <elias-marian@web.de>
2022-01-29 13:44:02 +0100

    :bug: Fixed transforming messages

--- src/main/kotlin/com/github/myraBot/diskord/common/entities/applicationCommands/InteractionCallbackData.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/entities/applicationCommands/InteractionCallbackData.kt
@@ -4,25 +4,27 @@ import com.github.myraBot.diskord.common.entities.applicationCommands.components
 import com.github.myraBot.diskord.common.entities.message.Attachment
 import com.github.myraBot.diskord.common.entities.message.embed.Embed
 import com.github.myraBot.diskord.rest.builders.MessageBuilder
+import com.github.myraBot.diskord.rest.transform
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class InteractionCallbackData(
-        val tts: Boolean? = null,
-        val content: String? = null,
-        val embeds: MutableList<Embed>? = null,
-        val components: MutableList<Component>? = null,
-        val attachments: MutableList<Attachment>? = null
+    val tts: Boolean? = null,
+    val content: String? = null,
+    val embeds: MutableList<Embed>? = null,
+    val components: MutableList<Component>? = null,
+    val attachments: MutableList<Attachment>? = null
 ) {
     companion object {
-
-        fun fromMessageBuilder(messageBuilder: MessageBuilder): InteractionCallbackData = InteractionCallbackData(
-            tts = messageBuilder.tts ?: false,
-            content = messageBuilder.content,
-            embeds = messageBuilder.embeds,
-            components = messageBuilder.actionRows,
-        )
-
+        suspend fun fromMessageBuilder(messageBuilder: MessageBuilder): InteractionCallbackData =
+            messageBuilder.transform().let { builder ->
+                InteractionCallbackData(
+                    tts = builder.tts ?: false,
+                    content = builder.content,
+                    embeds = builder.embeds,
+                    components = builder.actionRows,
+                )
+            }
     }
 
 }
