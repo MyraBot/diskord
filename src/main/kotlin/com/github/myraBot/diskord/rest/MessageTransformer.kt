55cb588e49dbb9dc7ab25419c43435f04d9e1ee9
Marian <elias-marian@web.de>
2022-01-29 13:05:02 +0100

    :sparkles: Added MessageTransformer#onText back

--- src/main/kotlin/com/github/myraBot/diskord/rest/MessageTransformer.kt
+++ src/main/kotlin/com/github/myraBot/diskord/rest/MessageTransformer.kt
@@ -8,17 +8,31 @@ import com.github.myraBot.diskord.rest.builders.MessageBuilder
 interface MessageTransformer {
     suspend fun onEmbed(embed: Embed): Embed
     suspend fun onInteraction(interaction: Interaction, embed: Embed): Embed
+    suspend fun onText(text: String): String
 }
 
 object DefaultTransformer : MessageTransformer {
     override suspend fun onEmbed(embed: Embed): Embed = embed
     override suspend fun onInteraction(interaction: Interaction, embed: Embed): Embed = embed
+    override suspend fun onText(text: String): String = text
 }
 
 suspend fun MessageBuilder.transform(): MessageBuilder {
     val t = DiskordBuilder.transformer
+
+    content?.apply { t.onText(this) }
     embeds.onEach { embed ->
         t.onEmbed(embed)
+
+        embed.title?.apply { t.onText(this) }
+        embed.author?.name?.apply { t.onText(this) }
+        embed.description?.apply { t.onText(this) }
+        embed.fields.forEach { field ->
+            field.name.apply { t.onText(this) }
+            field.value.apply { t.onText(this) }
+        }
+        embed.footer?.text?.apply { t.onText(this) }
     }
+
     return this
 }
