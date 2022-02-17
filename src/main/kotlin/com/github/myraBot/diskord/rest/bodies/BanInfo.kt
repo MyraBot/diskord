3cdc3eec2f0518a97b770ddc7636beaff0a6a23d
Marian <elias-marian@web.de>
2022-02-17 13:05:21 +0100

    :bug: Made BanInfo serializable

--- src/main/kotlin/com/github/myraBot/diskord/rest/bodies/BanInfo.kt
+++ src/main/kotlin/com/github/myraBot/diskord/rest/bodies/BanInfo.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.SerialName
 /**
  * @property deleteMessageDays The number of days to delete messages for (0-7).
  */
+@kotlinx.serialization.Serializable
 data class BanInfo(
     @SerialName("delete_message_days") val deleteMessageDays: Int? = null
 )
