b7e570853ed8badf7dbd37fc54f167a2eb794838
Marian <elias-marian@web.de>
2022-02-17 12:07:52 +0100

    :sparkles: Added GenericInteractionCreateEvent#user

--- src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/interactions/GenericInteractionCreateEvent.kt
+++ src/main/kotlin/com/github/myraBot/diskord/gateway/listeners/impl/interactions/GenericInteractionCreateEvent.kt
@@ -1,5 +1,6 @@
 package com.github.myraBot.diskord.gateway.listeners.impl.interactions
 
+import com.github.myraBot.diskord.common.entities.User
 import com.github.myraBot.diskord.common.entities.applicationCommands.Interaction
 import com.github.myraBot.diskord.gateway.listeners.Event
 import com.github.myraBot.diskord.rest.behaviors.InteractionCreateBehavior
@@ -7,7 +8,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 abstract class GenericInteractionCreateEvent(
-        open val data: Interaction,
+    open val data: Interaction,
 ) : Event(), InteractionCreateBehavior {
     override val interaction: Interaction get() = data
+
+    val user: User get() = data.member?.user ?: data.user.forceValue
 }
