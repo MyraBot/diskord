5d59d0e590a0a08284de5599e3c04af98c996589
Marian <elias-marian@web.de>
2022-02-18 16:02:28 +0100

    :sparkles: Added User#getFlags

--- src/main/kotlin/com/github/myraBot/diskord/common/entities/User.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/entities/User.kt
@@ -1,8 +1,10 @@
 package com.github.myraBot.diskord.common.entities
 
+import com.github.myraBot.diskord.common.Diskord
 import com.github.myraBot.diskord.common.Optional
 import com.github.myraBot.diskord.common.entities.channel.DmChannel
 import com.github.myraBot.diskord.common.entities.user.UserFlag
+import com.github.myraBot.diskord.common.isMissing
 import com.github.myraBot.diskord.common.toJson
 import com.github.myraBot.diskord.rest.CdnEndpoints
 import com.github.myraBot.diskord.rest.Endpoints
@@ -24,16 +26,26 @@ class User(
     @SerialName("bot") val isBot: Boolean = false,
     val system: Boolean = false,
     @SerialName("mfa_enabled") val mfaEnabled: Boolean = false,
-    @Serializable(with = UserFlag.Serializer::class) @SerialName("public_flags") val flags: Optional<List<UserFlag>> = Optional.Missing()
+    private @Serializable(with = UserFlag.Serializer::class) @SerialName("public_flags") val flags: Optional<List<UserFlag>> = Optional.Missing()
 ) {
     val avatar: String
         get() = avatarHash?.let {
             CdnEndpoints.userAvatar.apply { arg("user_id", id); arg("user_avatar", avatarHash) }
         } ?: CdnEndpoints.defaultUserAvatar.apply { arg("user_discriminator", discriminator.toInt() % 5) }
+
     val asTag: String get() = "$username#$discriminator"
+
     val mention: String get() = Mention.user(id)
+
     val link: String get() = "https://discord.com/users/$id"
+
     suspend fun openDms(): Promise<DmChannel> = Promise
         .of(Endpoints.createDm) { json = DmCreation(id).toJson() }
         .map { data -> data?.let { DmChannel(it) } }
+
+    suspend fun getFlags(): Promise<List<UserFlag>> {
+        return if (!flags.isMissing()) Promise.of(flags.forceValue)
+        else Diskord.getUser(id).mapNonNull { it.flags.forceValue }
+    }
+
 }
