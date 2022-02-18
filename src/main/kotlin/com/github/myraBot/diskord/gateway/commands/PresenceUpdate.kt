c5be27a3c5d2380d95a1222d82ab51d8d3d96afe
Marian <elias-marian@web.de>
2022-02-18 23:49:27 +0100

    :memo: Added documentation

--- src/main/kotlin/com/github/myraBot/diskord/gateway/commands/PresenceUpdate.kt
+++ src/main/kotlin/com/github/myraBot/diskord/gateway/commands/PresenceUpdate.kt
@@ -1,5 +1,6 @@
 package com.github.myraBot.diskord.gateway.commands
 
+import com.github.myraBot.diskord.common.Diskord
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.descriptors.PrimitiveKind
@@ -8,6 +9,15 @@ import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
+/**
+ * Presence builder, used to update the presence and status of the bot.
+ * Used in [Diskord.updatePresence].
+ *
+ * @property since The unix time (in milliseconds) of when the client went idle, or null if the client is not idle.
+ * @property activity The bots activity.
+ * @property status The bots online status.
+ * @property afk Whether the bot is afk.
+ */
 data class Presence(
     val since: Int? = null,
     var activity: Activity? = null,
@@ -15,6 +25,16 @@ data class Presence(
     var afk: Boolean = false
 )
 
+/**
+ * [Documentation](https://discord.com/developers/docs/topics/gateway#update-presence-gateway-presence-update-structure)
+ * Opt-code data for updating the presence and status of the bot.
+ * Can be updated using [Diskord.updatePresence].
+ *
+ * @property since The unix time (in milliseconds) of when the client went idle, or null if the client is not idle.
+ * @property activities The bots activities.
+ * @property status The bots online status.
+ * @property afk Whether the bot is afk.
+ */
 @Serializable
 data class PresenceUpdate(
     val since: Int? = null,
@@ -23,6 +43,14 @@ data class PresenceUpdate(
     var afk: Boolean = false
 )
 
+/**
+ * [Documentation](https://discord.com/developers/docs/topics/gateway#activity-object-activity-structure)
+ * Activity of the bot. Can be set using [Diskord.updatePresence].
+ *
+ * @property name The activity's name.
+ * @property type A [ActivityType].
+ * @property url stream url, is validated when type is [ActivityType.STREAMING].
+ */
 @Serializable
 data class Activity(
     val name: String,
@@ -30,6 +58,12 @@ data class Activity(
     val url: String? = null
 )
 
+/**
+ * [Documentation](https://discord.com/developers/docs/topics/gateway#update-presence-status-types)
+ * Possible online status types of the bot.
+ *
+ * @property status Status name.
+ */
 @Serializable(with = Status.Serializer::class)
 enum class Status(val status: String) {
     ONLINE("online"),
@@ -45,6 +79,12 @@ enum class Status(val status: String) {
     }
 }
 
+/**
+ * [Documentation](https://discord.com/developers/docs/topics/gateway#activity-object-activity-types)
+ * Possible activity types for the bot.
+ *
+ * @property id Activity id.
+ */
 @Serializable(with = ActivityType.Serializer::class)
 enum class ActivityType(val id: Int) {
     GAME(0),
