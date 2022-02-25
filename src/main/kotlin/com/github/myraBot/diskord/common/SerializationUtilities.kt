36855fb41e1adce2ef3f59a3b6d8b26579fd2344
Marian <elias-marian@web.de>
2022-02-25 20:29:17 +0100

    :children_crossing: Made serialization utilities internal

--- src/main/kotlin/com/github/myraBot/diskord/common/SerializationUtilities.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/SerializationUtilities.kt
@@ -6,23 +6,23 @@ import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.encodeToJsonElement
 import kotlinx.serialization.json.jsonObject
 
-val JSON = Json {
+internal val JSON = Json {
     ignoreUnknownKeys = true
     encodeDefaults = true
     explicitNulls = false
 }
-val JSON_WITH_NULLS = Json {
+internal val JSON_WITH_NULLS = Json {
     ignoreUnknownKeys = true
     encodeDefaults = true
     explicitNulls = true
 }
 
-inline fun <reified T> T.toJson(explicitNulls: Boolean = false): String {
+internal inline fun <reified T> T.toJson(explicitNulls: Boolean = false): String {
     return if (explicitNulls) JSON_WITH_NULLS.encodeToString(this)
     else JSON.encodeToString(this)
 }
 
-inline fun <reified T> T.toJsonObj(explicitNulls: Boolean = false): JsonObject {
+internal inline fun <reified T> T.toJsonObj(explicitNulls: Boolean = false): JsonObject {
     return if (explicitNulls) JSON_WITH_NULLS.encodeToJsonElement(this).jsonObject
     else JSON.encodeToJsonElement(this).jsonObject
 }
