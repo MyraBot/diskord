3e55376bd6a81ae97a94720bff74e9fead5a94f2
Marian <elias-marian@web.de>
2022-02-25 21:37:07 +0100

    :pencil2: Fixed typo in file name

--- src/main/kotlin/com/github/myraBot/diskord/gateway/handler/OpCode.kt
+++ src/main/kotlin/com/github/myraBot/diskord/gateway/handler/OpCode.kt
@@ -3,8 +3,6 @@ package com.github.myraBot.diskord.gateway
 import com.github.myraBot.diskord.common.JSON
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.encodeToJsonElement
