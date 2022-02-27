69230c7245128c3f100800bf6515e8687c6652fa
Marian <elias-marian@web.de>
2022-02-27 17:46:03 +0100

    :fire: Removed DefaultBehavior#diskord

--- src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/DefaultBehavior.kt
+++ src/main/kotlin/com/github/myraBot/diskord/rest/behaviors/DefaultBehavior.kt
@@ -1,12 +1,12 @@
 package com.github.myraBot.diskord.rest.behaviors
 
-import com.github.myraBot.diskord.common.Diskord
 import com.github.myraBot.diskord.common.entities.Application
 import com.github.myraBot.diskord.rest.Endpoints
 import com.github.myraBot.diskord.rest.request.RestClient
 import kotlinx.coroutines.Deferred
 
 interface DefaultBehavior {
+
     fun getApplicationAsync(): Deferred<Application> = RestClient.executeAsync(Endpoints.getBotApplication)
-    val diskord: Diskord get() = Diskord
+
 }
