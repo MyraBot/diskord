a24b7a9dca6ef7880e372e5ea98ba47aee8757da
Marian <elias-marian@web.de>
2022-03-01 15:56:06 +0100

    :wastebasket: Cleaned up imports

--- src/main/kotlin/com/github/myraBot/diskord/common/utilities/BuildedThings.kt
+++ src/main/kotlin/com/github/myraBot/diskord/common/utilities/BuildedThings.kt
@@ -1,12 +1,8 @@
 package com.github.myraBot.diskord.common.utilities
 
-import com.github.myraBot.diskord.common.Diskord
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
-import io.ktor.client.features.*
 import io.ktor.client.features.websocket.*
-import io.ktor.client.request.*
-import kotlinx.serialization.json.Json
 
 val GATEWAY_CLIENT = HttpClient(CIO) {
     install(WebSockets)
