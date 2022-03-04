7044e8c812c7817f3ac5a63fceb601b71d3aa49e
Marian <elias-marian@web.de>
2022-03-04 15:47:46 +0100

    :art: Redid MessageFlags

--- src/main/kotlin/bot/myra/diskord/common/entities/message/MessageFlags.kt
+++ src/main/kotlin/bot/myra/diskord/common/entities/message/MessageFlags.kt
@@ -8,31 +8,25 @@ import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
-@Serializable(with = MessageFlags.Serializer::class)
-class MessageFlags(val code: Int) {
-    operator fun contains(flag: MessageFlag) = flag.code and this.code == flag.code
+@Serializable(with = MessageFlags.MessageFlagsSerializer::class)
+class MessageFlags(flags: MutableList<MessageFlag>) : MutableList<MessageFlag> by flags {
 
-    internal object Serializer : KSerializer<MessageFlags> {
-        override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("message_flags", PrimitiveKind.INT)
-        override fun deserialize(decoder: Decoder): MessageFlags = MessageFlags(decoder.decodeInt())
-        override fun serialize(encoder: Encoder, value: MessageFlags) = encoder.encodeInt(value.code)
-    }
-}
+    internal object MessageFlagsSerializer : KSerializer<MessageFlags> {
+        override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("message_flag", PrimitiveKind.INT)
 
-object MessageFlagsSerializer : KSerializer<MutableList<MessageFlag>> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("message_flag", PrimitiveKind.INT)
-
-    override fun deserialize(decoder: Decoder): MutableList<MessageFlag> {
-        val flags = mutableListOf<MessageFlag>()
-        decoder.decodeInt().let { bit ->
-            flags.add(MessageFlag.values().first { bit and it.code == it.code })
+        override fun deserialize(decoder: Decoder): MessageFlags {
+            val flags = mutableListOf<MessageFlag>()
+            decoder.decodeInt().let { bit ->
+                flags.add(MessageFlag.values().first { bit and it.code == it.code })
+            }
+            return MessageFlags(flags)
         }
-        return flags
-    }
 
-    override fun serialize(encoder: Encoder, value: MutableList<MessageFlag>) {
-        val bitCode = value.map { 1 shl it.code }.reduce { acc, i -> acc or i }
-        encoder.encodeInt(bitCode)
+        override fun serialize(encoder: Encoder, value: MessageFlags) {
+            val bitCode = value.map { 1 shl it.code }.reduce { acc, i -> acc or i }
+            encoder.encodeInt(bitCode)
+        }
+
     }
 
 }
