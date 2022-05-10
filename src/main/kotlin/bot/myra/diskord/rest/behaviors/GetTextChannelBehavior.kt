bc4fd6c62881131d8c0af503a69181e94702c6c0
Marian <elias-marian@web.de>
2022-05-10 18:19:34 +0200

    :recycle: Refactored code

--- src/main/kotlin/bot/myra/diskord/rest/behaviors/GetTextChannelBehavior.kt
+++ src/main/kotlin/bot/myra/diskord/rest/behaviors/GetTextChannelBehavior.kt
@@ -11,10 +11,10 @@ interface GetTextChannelBehavior
 suspend inline fun <reified T> GetTextChannelBehavior.getChannel(id: String): T? {
     val data: ChannelData = EntityProvider.getChannel(id) ?: return null
     return when (T::class) {
-        ChannelData::class -> data
-        DmChannel::class -> DmChannel(data)
-        TextChannel::class -> TextChannel(data)
+        ChannelData::class  -> data
+        DmChannel::class    -> DmChannel(data)
+        TextChannel::class  -> TextChannel(data)
         VoiceChannel::class -> VoiceChannel(data)
-        else -> throw IllegalStateException("Unknown channel to cast: ${T::class.simpleName}")
+        else                -> throw IllegalStateException("Unknown channel to cast: ${T::class.simpleName}")
     } as T
 }
