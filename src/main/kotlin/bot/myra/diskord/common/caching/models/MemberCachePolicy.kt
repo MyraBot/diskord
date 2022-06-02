a71aa1ac8709717f14b8f5a6e3ad098aecf8ee50
marian <elias-marian@web.de>
2022-06-02 12:21:08 +0000

    :zap: Added missing member caching

--- src/main/kotlin/bot/myra/diskord/common/caching/models/MemberCachePolicy.kt
+++ src/main/kotlin/bot/myra/diskord/common/caching/models/MemberCachePolicy.kt
@@ -4,12 +4,20 @@ import bot.myra.diskord.common.caching.DoubleKey
 import bot.myra.diskord.common.caching.GenericCachePolicy
 import bot.myra.diskord.common.entities.guild.Member
 import bot.myra.diskord.gateway.events.ListenTo
+import bot.myra.diskord.gateway.events.impl.guild.MemberJoinEvent
+import bot.myra.diskord.gateway.events.impl.guild.MemberRemoveEvent
 import bot.myra.diskord.gateway.events.impl.guild.MemberUpdateEvent
 
 class MemberCachePolicy : GenericCachePolicy<DoubleKey<String, String>, Member>() {
 
+    @ListenTo(MemberJoinEvent::class)
+    fun onMemberJoin(event: MemberJoinEvent) = update(event.member)
+
     @ListenTo(MemberUpdateEvent::class)
     fun onMemberUpdate(event: MemberUpdateEvent) = update(event.member)
+
+    @ListenTo(MemberRemoveEvent::class)
+    fun onMemberRemove(event: MemberRemoveEvent) = remove(DoubleKey(event.removedMember.guildId, event.removedMember.user.id))
 
 }
 
