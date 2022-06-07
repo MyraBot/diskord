534fc3e7427e054141e6e334eb7e701cb4ff4ebd
marian <elias-marian@web.de>
2022-06-07 11:55:31 +0000

    :adhesive_bandage: Removed parallel member retrieving

--- src/main/kotlin/bot/myra/diskord/common/entities/channel/VoiceChannel.kt
+++ src/main/kotlin/bot/myra/diskord/common/entities/channel/VoiceChannel.kt
@@ -2,23 +2,15 @@ package bot.myra.diskord.common.entities.channel
 
 import bot.myra.diskord.common.Diskord
 import bot.myra.diskord.common.entities.guild.Member
-import bot.myra.diskord.rest.request.RestClient
 import bot.myra.diskord.voice.VoiceChannelBehavior
-import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.launch
 
 data class VoiceChannel(
     override val data: ChannelData,
 ) : VoiceChannelBehavior, GuildChannel {
 
-    suspend fun getMembers(): List<Member> {
+    fun getMembers(): List<Member> {
         val voiceStates = Diskord.cachePolicy.voiceStateCache.view().filter { it.channelId === data.id }
-        return voiceStates.map {
-            val future = CompletableDeferred<Member?>()
-            RestClient.coroutineScope.launch { future.complete(it.getMember()) }
-            future
-        }.awaitAll().filterNotNull()
+        return voiceStates.mapNotNull { it.getMember() }
     }
 
 }
