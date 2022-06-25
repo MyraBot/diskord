9dacc5a516f58af0dd0fff1389efe0033541b295
marian <elias-marian@web.de>
2022-06-25 17:15:20 +0000

    :fire: Removed role cache policy

--- src/main/kotlin/bot/myra/diskord/common/cache/models/RoleCachePolicy.kt
+++
@@ -1,6 +0,0 @@
-package bot.myra.diskord.common.cache.models
-
-import bot.myra.diskord.common.cache.GenericCachePolicy
-import bot.myra.diskord.common.entities.guild.Role
-
-class RoleCachePolicy : GenericCachePolicy<String, Role>()
