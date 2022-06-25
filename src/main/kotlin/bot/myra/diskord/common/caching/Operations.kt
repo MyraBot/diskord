df95e54b80d5941fe1ac160bf4b605ac385ba954
marian <elias-marian@web.de>
2022-06-25 16:27:52 +0000

    :sparkles: Added cache modification

--- src/main/kotlin/bot/myra/diskord/common/caching/Operations.kt
+++ src/main/kotlin/bot/myra/diskord/common/caching/Operations.kt
@@ -3,6 +3,7 @@ package bot.myra.diskord.common.caching
 typealias ViewCache<V> = () -> List<V>
 typealias GetCache<K, V> = (K) -> V?
 typealias UpdateCache<V> = (V) -> Unit
+typealias ModifyCache<V> = (V) -> V
 typealias RemoveCache<K> = (K) -> Unit
 typealias ViewByGuildId<K, V> = (K) -> List<V>?
 typealias AssociatedByGuildId<ITEM_ID> = (ITEM_ID) -> String?
