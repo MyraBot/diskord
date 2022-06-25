67294a019ecc5bfc77f890d57aeecf24b95a7f30
marian <elias-marian@web.de>
2022-06-25 18:18:06 +0000

    :card_file_box: Added gateway events for role caching

--- src/main/kotlin/bot/myra/diskord/common/cache/models/GuildCachePolicy.kt
+++ src/main/kotlin/bot/myra/diskord/common/cache/models/GuildCachePolicy.kt
@@ -5,6 +5,9 @@ import bot.myra.diskord.common.cache.GenericCachePolicy
 import bot.myra.diskord.common.entities.guild.Guild
 import bot.myra.diskord.gateway.events.ListenTo
 import bot.myra.diskord.gateway.events.impl.guild.GenericGuildCreateEvent
+import bot.myra.diskord.gateway.events.impl.guild.roles.RoleCreateEvent
+import bot.myra.diskord.gateway.events.impl.guild.roles.RoleDeleteEvent
+import bot.myra.diskord.gateway.events.impl.guild.roles.RoleUpdateEvent
 
 class GuildCachePolicy : GenericCachePolicy<String, Guild>() {
 
@@ -13,6 +16,24 @@ class GuildCachePolicy : GenericCachePolicy<String, Guild>() {
         Diskord.guildIds.add(event.guild.id)
         update(event.guild)
         Diskord.pendingGuilds[event.guild.id]?.complete(event.guild)
+    }
+
+    @ListenTo(RoleCreateEvent::class)
+    fun onRoleCreate(event: RoleCreateEvent) = get(event.guildId)?.apply {
+        roles = roles.toMutableList().apply { add(event.role) }
+    }
+
+    @ListenTo(RoleUpdateEvent::class)
+    fun onRoleUpdate(event: RoleUpdateEvent) = get(event.guildId)?.apply {
+        roles = roles.toMutableList().apply {
+            removeIf { it.id == event.role.id }
+            add(event.role)
+        }
+    }
+
+    @ListenTo(RoleDeleteEvent::class)
+    fun onRoleDelete(event: RoleDeleteEvent) = get(event.guildId)?.apply {
+        roles = roles.toMutableList().apply { removeIf { it.id == event.roleId } }
     }
 
 }
