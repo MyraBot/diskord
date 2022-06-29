b194f6249f70e7ccb438ecf3aeacfca42a4f1fb5
marian <elias-marian@web.de>
2022-06-29 12:57:01 +0000

    :label: Fixed DefaultGuildCache#policy return type

--- src/main/kotlin/bot/myra/diskord/common/cache/defaultPolicy/DefaultGuildCache.kt
+++ src/main/kotlin/bot/myra/diskord/common/cache/defaultPolicy/DefaultGuildCache.kt
@@ -1,13 +1,14 @@
 package bot.myra.diskord.common.cache.defaultPolicy
 
 import bot.myra.diskord.common.cache.models.GuildCachePolicy
+import bot.myra.diskord.common.cache.models.MutableGuildCachePolicy
 import bot.myra.diskord.common.entities.guild.Guild
 
 class DefaultGuildCache {
 
     private val map = mutableMapOf<String, Guild>()
 
-    fun policy(): GuildCachePolicy = GuildCachePolicy().apply {
+    fun policy(): GuildCachePolicy = MutableGuildCachePolicy().apply {
         view { map.values.toList() }
         get { map[it] }
         update { map[it.id] = it }
