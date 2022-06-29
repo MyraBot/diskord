e9452601363fa870e24f6efb234e747306cdf874
marian <elias-marian@web.de>
2022-06-29 12:20:05 +0000

    :coffin: Removed unused cache operation

--- src/main/kotlin/bot/myra/diskord/common/cache/Operations.kt
+++ src/main/kotlin/bot/myra/diskord/common/cache/Operations.kt
@@ -3,7 +3,6 @@ package bot.myra.diskord.common.cache
 typealias ViewCache<V> = () -> List<V>
 typealias GetCache<K, V> = (K) -> V?
 typealias UpdateCache<V> = (V) -> Unit
-typealias ModifyCache<V> = (V) -> V
 typealias RemoveCache<K> = (K) -> Unit
 typealias ViewByGuildId<K, V> = (K) -> List<V>?
 typealias AssociatedByGuildId<ITEM_ID> = (ITEM_ID) -> String?
