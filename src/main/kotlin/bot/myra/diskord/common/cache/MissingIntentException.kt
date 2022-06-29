cb02392de4fd1f118b39eb6e41aadc2656282f45
marian <elias-marian@web.de>
2022-06-29 12:26:17 +0000

    :memo: Added documentation

--- src/main/kotlin/bot/myra/diskord/common/cache/MissingIntentException.kt
+++ src/main/kotlin/bot/myra/diskord/common/cache/MissingIntentException.kt
@@ -3,4 +3,11 @@ package bot.myra.diskord.common.cache
 import bot.myra.diskord.gateway.GatewayIntent
 import kotlin.reflect.KClass
 
+/**
+ * Gets thrown when a cache is used, which requires a specific intent.
+ * Either disable the cache or enable the intent.
+ *
+ * @param cache The superclass of the cache.
+ * @param intent The intent which is required for the cache.
+ */
 class MissingIntentException(cache: KClass<*>, intent: GatewayIntent) : Exception("${cache.simpleName} requires the ${intent.name} intent")
