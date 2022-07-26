995e904807bc7a9350b4ad3d8c77fa6f4b5dc457
marian <elias-marian@web.de>
2022-07-26 20:16:45 +0200

    :zap: Made Message cache update more efficient

--- src/main/kotlin/bot/myra/diskord/common/cache/models/MessageCachePolicy.kt
+++ src/main/kotlin/bot/myra/diskord/common/cache/models/MessageCachePolicy.kt
@@ -20,9 +20,34 @@ class MutableMessageCachePolicy : MessageCachePolicy() {
     @ListenTo(MessageCreateEvent::class)
     fun onMessageCreate(event: MessageCreateEvent) = update(event.message)
 
-    //TODO Improve this somehow
     @ListenTo(MessageUpdateEvent::class)
-    suspend fun onMessageUpdate(event: MessageUpdateEvent) = update(Diskord.getMessage(event.message.channelId, event.message.id)!!)
+    suspend fun onMessageUpdate(event: MessageUpdateEvent) {
+        val message = get(event.message.id) ?: Diskord.getMessage(event.message.channelId, event.message.id) ?: return
+        val updatedMessage = Message(
+            id = message.id,
+            channelId = message.channelId,
+            guildId = message.guildId,
+            user = message.user,
+            memberData = message.memberData,
+            content = event.message.content ?: message.content,
+            timestamp = message.timestamp,
+            edited = event.message.edited,
+            tts = message.tts,
+            mentionsEveryone = message.mentionsEveryone,
+            mentionedUsers = message.mentionedUsers,
+            mentionedRoles = message.mentionedRoles,
+            mentionedChannels = message.mentionedChannels,
+            attachments = event.message.attachments ?: message.attachments,
+            embeds = event.message.embeds?.toMutableList() ?: message.embeds,
+            reactions = message.reactions,
+            pinned = message.pinned,
+            webhookId = message.webhookId,
+            type = message.type,
+            flags = message.flags,
+            components = event.message.components?.toMutableList() ?: message.components
+        )
+        update(updatedMessage)
+    }
 
     @ListenTo(MessageDeleteEvent::class)
     fun onMessageDelete(event: MessageDeleteEvent) = remove(event.message.id)
--- src/main/kotlin/bot/myra/diskord/common/entities/message/UpdatedMessage.kt
+++ src/main/kotlin/bot/myra/diskord/common/entities/message/UpdatedMessage.kt
@@ -1,8 +1,12 @@
 package bot.myra.diskord.common.entities.message
 
+import bot.myra.diskord.common.entities.applicationCommands.components.Component
+import bot.myra.diskord.common.entities.message.embed.Embed
+import bot.myra.diskord.common.serializers.SInstant
 import bot.myra.diskord.rest.behaviors.Entity
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import java.time.Instant
 
 /**
  * @property id The message id.
@@ -13,5 +17,10 @@ import kotlinx.serialization.Serializable
 data class UpdatedMessage(
     override val id: String,
     @SerialName("channel_id") val channelId: String,
-    @SerialName("guild_id") val guildId: String? = null
+    val content: String? = null,
+    @Serializable(with = SInstant::class) @SerialName("edited_timestamp") val edited: Instant,
+    @SerialName("guild_id") val guildId: String? = null,
+    val attachments: List<Attachment>? = null,
+    val embeds: List<Embed>? = null,
+    val components: List<Component>? = null
 ) : Entity
