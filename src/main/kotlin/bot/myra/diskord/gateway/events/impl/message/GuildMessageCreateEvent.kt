80d99f98a8ca482ceaf000368cf88990317eba01
marian <elias-marian@web.de>
2022-08-30 20:21:40 +0200

    :bug: Fixed invalid function call

--- src/main/kotlin/bot/myra/diskord/gateway/events/impl/message/GuildMessageCreateEvent.kt
+++ src/main/kotlin/bot/myra/diskord/gateway/events/impl/message/GuildMessageCreateEvent.kt
@@ -10,9 +10,9 @@ import bot.myra.diskord.gateway.events.Event
 class GuildMessageCreateEvent(
     val message: Message
 ) : Event() {
+    val member: Member = message.member!!
 
     suspend fun getGuild(): Guild = message.getGuild()!!
-    fun getMember(): Member = message.getMember()!!
     suspend fun getChannel(): TextChannel = message.getChannelAs()!!
 
 }
