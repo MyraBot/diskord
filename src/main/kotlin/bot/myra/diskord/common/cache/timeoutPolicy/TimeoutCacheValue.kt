2799a1193f0d2e0d965414a7f564c55021bda407
marian <elias-marian@web.de>
2022-09-03 17:19:39 +0200

    :sparkles: Added timeout cache policies

---
+++ src/main/kotlin/bot/myra/diskord/common/cache/timeoutPolicy/TimeoutCacheValue.kt
@@ -0,0 +1,14 @@
+package bot.myra.diskord.common.cache.timeoutPolicy
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlin.time.Duration.Companion.seconds
+
+data class TimeoutCacheValue<T>(
+    var value: T,
+    var expireAt: Instant = Clock.System.now().plus(10.seconds)
+) {
+    fun updateExpiry() {
+        expireAt = Clock.System.now().plus(10.seconds)
+    }
+}
