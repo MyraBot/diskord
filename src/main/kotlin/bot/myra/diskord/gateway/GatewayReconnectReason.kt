b98f0b9e3d19b3eb8d73889ce313642bfe594e41
marian <elias-marian@web.de>
2022-11-11 11:53:47 +0100

    :bug: Fixed resuming on invalid session

--- src/main/kotlin/bot/myra/diskord/gateway/GatewayReconnectReason.kt
+++ src/main/kotlin/bot/myra/diskord/gateway/GatewayReconnectReason.kt
@@ -9,7 +9,7 @@ package bot.myra.diskord.gateway
 abstract class ReconnectReason(val resume: Boolean, val cause: String) {
     class NoCode : ReconnectReason(true, "No close code found")
     class InvalidSessionResume : ReconnectReason(true, "Invalid session (resumable)")
-    class InvalidSession : ReconnectReason(true, "Invalid session")
+    class InvalidSession : ReconnectReason(false, "Invalid session")
     class RequestReconnect : ReconnectReason(true, "Received reconnect from discord")
     class UnknownCloseCode: ReconnectReason(false, "Unknown close code")
     class Any(resume: Boolean, cause: String) : ReconnectReason(resume, cause)
