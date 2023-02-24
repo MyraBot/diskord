8f99887a3a4631b6b6681ca5e2992665398c0e19
marian <elias-marian@web.de>
2023-02-24 15:58:30 +0100

    :sparkles: Added #getGuild

--- src/main/kotlin/bot/myra/diskord/gateway/events/impl/message/create/guild/MessageCreateGuildUserEvent.kt
+++ src/main/kotlin/bot/myra/diskord/gateway/events/impl/message/create/guild/MessageCreateGuildUserEvent.kt
@@ -1,8 +1,10 @@
 package bot.myra.diskord.gateway.events.impl.message.create.guild
 
 import bot.myra.diskord.common.Diskord
+import bot.myra.diskord.common.entities.guild.Guild
 import bot.myra.diskord.common.entities.guild.Member
 import bot.myra.diskord.common.entities.message.Message
+import bot.myra.diskord.rest.request.Result
 
 @Suppress("unused")
 class MessageCreateGuildUserEvent(
@@ -10,4 +12,5 @@ class MessageCreateGuildUserEvent(
     override val diskord: Diskord
 ) : GenericGuildMessageCreateEvent(message) {
     suspend fun getMember(): Member = message.getMember()!!
+    suspend fun getGuild(): Result<Guild> = message.getGuild()!!
 }
