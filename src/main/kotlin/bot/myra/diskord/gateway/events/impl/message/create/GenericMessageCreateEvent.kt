8cae61a38385af1fd1947b3e5ffb14c7c8769a74
marian <elias-marian@web.de>
2023-02-24 16:53:56 +0100

    :children_crossing: Added #getChannel function

--- src/main/kotlin/bot/myra/diskord/gateway/events/impl/message/create/GenericMessageCreateEvent.kt
+++ src/main/kotlin/bot/myra/diskord/gateway/events/impl/message/create/GenericMessageCreateEvent.kt
@@ -1,8 +1,11 @@
 package bot.myra.diskord.gateway.events.impl.message.create
 
+import bot.myra.diskord.common.entities.channel.text.GenericTextChannel
 import bot.myra.diskord.common.entities.message.Message
 import bot.myra.diskord.gateway.events.types.Event
 
 abstract class GenericMessageCreateEvent(
     open val message: Message
-) : Event()
+) : Event() {
+    suspend fun getChannel() = message.getChannelAs<GenericTextChannel>()
+}
