32a06daee011889ed65a357a78321818c131741a
marian <elias-marian@web.de>
2023-04-10 10:18:48 +0200

    :art: Removed unnecessary code because subclass already implements it

--- src/main/kotlin/bot/myra/diskord/gateway/events/impl/guild/create/GuildLazyLoadEvent.kt
+++ src/main/kotlin/bot/myra/diskord/gateway/events/impl/guild/create/GuildLazyLoadEvent.kt
@@ -6,14 +6,4 @@ import bot.myra.diskord.common.entities.guild.PossibleUnavailableGuild
 class GuildLazyLoadEvent(
     override val guild: PossibleUnavailableGuild,
     override val diskord: Diskord
-) : GenericGuildCreateEvent(guild) {
-
-    override suspend fun handle() {
-        diskord.unavailableGuilds.remove(guild.id)?.also {
-            guild.asExtendedGuild()?.asGuild()?.let { fullGuild ->
-                it.complete(fullGuild)
-            }
-        }
-    }
-
-}
+) : GenericGuildCreateEvent(guild)
