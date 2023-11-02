ccde45e3d1360cbfa9b48350e1c7a6fe2cc8030f
marian <elias-marian@web.de>
2023-11-02 11:01:17 +0100

    :adhesive_bandage: Fixed user data not updating

--- src/main/kotlin/bot/myra/diskord/common/cache/models/UserCachePolicy.kt
+++ src/main/kotlin/bot/myra/diskord/common/cache/models/UserCachePolicy.kt
@@ -2,11 +2,20 @@ package bot.myra.diskord.common.cache.models
 
 import bot.myra.diskord.common.cache.GenericCachePolicy
 import bot.myra.diskord.common.entities.user.UserData
+import bot.myra.diskord.gateway.events.ListenTo
+import bot.myra.diskord.gateway.events.impl.guild.MemberUpdateEvent
 
-class MutableUserCachePolicy : UserCachePolicy()
+class MutableUserCachePolicy : UserCachePolicy() {
+
+    @ListenTo(MemberUpdateEvent::class)
+    suspend fun onGuildMemberUpdate(event:MemberUpdateEvent) {
+        update(event.member.user.data)
+    }
+
+}
+
 class DisabledUserCachePolicy : UserCachePolicy()
 
 abstract class UserCachePolicy : GenericCachePolicy<String, UserData>() {
     override fun getAsKey(value: UserData): String = value.id
 }
-
